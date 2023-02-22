05237694f90b733e637184264b6f035c3280e626
Roman <roman.grabichler@gema-technik.de>
2023-02-22 10:22:05 +0100

    init

---
+++ LibraryVisionConnector/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace LibraryVision
+{
+    public class Entry
+    {
+        public static readonly string AmsId = "192.168.1.8.1.1";
+
+        public static LibraryVisionTwinController Plc { get; }
+
+            = new LibraryVisionTwinController(Tc3ConnectorAdapter.Create(AmsId, 851, true));
+    }
+}
---
+++ LibraryVisionExampleConnector/Entry.cs
@@ -0,0 +1,15 @@
+﻿using LibraryVision;
+using System;
+using Vortex.Adapters.Connector.Tc3.Adapter;
+
+namespace LibraryVisionExample
+{
+    public class Entry
+    {
+        public static readonly string AmsId = "192.168.1.8.1.1";
+
+        public static LibraryVisionExampleTwinController Plc { get; }
+
+            = new LibraryVisionExampleTwinController(Tc3ConnectorAdapter.Create(AmsId, 852, true));
+    }
+}
